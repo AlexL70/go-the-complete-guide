387653acfa75225cf4afc4b8370459b099907775
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-05-28 16:36:01 +0300

    33. Understanding Functions

--- basics/investment_calculator.go
+++ basics/investment_calculator.go
@@ -9,15 +9,19 @@ func main() {
 	const inflationRate = 2.0 // Annual inflation rate in percentage
 	var investmentAmount, expectedReturnRate, years float64
 
-	fmt.Print("Enter the amount you want to invest: $")
+	outputText("Enter the amount you want to invest: $")
 	fmt.Scan(&investmentAmount)
-	fmt.Print("Enter the expected annual return rate (in percentage): ")
+	outputText("Enter the expected annual return rate (in percentage): ")
 	fmt.Scan(&expectedReturnRate)
-	fmt.Print("Enter the number of years you want to invest for: ")
+	outputText("Enter the number of years you want to invest for: ")
 	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	output := fmt.Sprintf("Future Value of Investment: $%.2f\nFuture Value adjusted to inflation rate: $%.2f\n", futureValue, futureRealValue)
 	fmt.Print(output)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
