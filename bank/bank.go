53ab89a0d486ffd768cf2dfd35393158d1ae6248
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-05-28 17:28:47 +0300

    38. Introducing if Statements & Booleans

--- bank/bank.go
+++ bank/bank.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance float64 = 1000.00 // Initial balance
+
 	fmt.Println("Welcome to the Bank Management System")
 	fmt.Println("What would you like to do?")
 	fmt.Println("1. Check balance")
@@ -13,5 +15,14 @@ func main() {
 	var choice int
 	fmt.Println("Please enter your choice (1-4):")
 	fmt.Scanln(&choice)
-	fmt.Println("You chose option:", choice)
+	if choice < 1 || choice > 4 {
+		fmt.Println("Invalid choice. Please enter a number between 1 and 4.")
+		return
+	}
+	switch choice {
+	case 1:
+		fmt.Printf("Your current balance is: $%.2f\n", accountBalance)
+	default:
+		fmt.Println("Thank you for using the Bank Management System. Goodbye!")
+	}
 }
