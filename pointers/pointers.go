f609f0f7e1b24ca94ed251662e7862afd9a0a2f2
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-05-30 16:28:17 +0300

    67. Pointers as Values

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	age := 32 // regular variable
-	fmt.Printf("Age: %d\n", age)
+	age := 32          // regular variable
+	agePointer := &age // pointer to the variable
+
+	fmt.Printf("Age: %d\n", *agePointer)
+	fmt.Printf("Age Pointer: %p\n", agePointer)
 	fmt.Printf("Adult Years: %d\n", getAdultYears(age))
 }
 
