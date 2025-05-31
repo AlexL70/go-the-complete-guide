111f4df5409f53b77fd91c97542f65c04dd50056
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-05-31 13:24:10 +0300

    84. Using Constructor Functions For Validation

--- structs/structs.go
+++ structs/structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -8,13 +9,30 @@ import (
 type User struct {
 	FirstName string
 	LastName  string
-	BirthDate string
+	BirthDate time.Time // Using time.Time for date representation
 	CreatedAt time.Time
+}
+
+// Constructors are not a common pattern in Go, but we can use methods to initialize structs.
+func NewUser(firstName, lastName, birthDate string) (*User, error) {
+	if firstName == "" || lastName == "" {
+		return nil, errors.New("First name and last name cannot be empty")
+	}
+	var date, err = time.Parse("01/02/2006", birthDate)
+	if err != nil {
+		return nil, fmt.Errorf("please enter date in the next format \"MM/DD/YYYY\"; error parsing date: %v", err)
+	}
+	return &User{
+		FirstName: firstName,
+		LastName:  lastName,
+		BirthDate: date,
+		CreatedAt: time.Now(),
+	}, nil
 }
 
 func (u User) printUserData() {
 	fmt.Printf("User: %s %s, Birth Date: %s, Created At: %s\n",
-		u.FirstName, u.LastName, u.BirthDate, u.CreatedAt.Format(time.RFC3339))
+		u.FirstName, u.LastName, u.BirthDate.Format("DD of MMM, YYYY"), u.CreatedAt.Format(time.RFC3339))
 }
 
 // To update structure fields, we must use a pointer receiver
@@ -30,13 +48,12 @@ func main() {
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthDate := getUserData("Please enter your birth date (MM/DD/YYYY): ")
 
-	var appUser User = User{
-		FirstName: userFirstName,
-		LastName:  userLastName,
-		BirthDate: userBirthDate,
-		CreatedAt: time.Now(),
+	var appUser *User
+	appUser, err := NewUser(userFirstName, userLastName, userBirthDate)
+	if err != nil {
+		fmt.Println("Error creating user:", err)
+		return
 	}
-
 	appUser.printUserData()
 	appUser.clearUserName()
 	appUser.printUserData()
@@ -45,6 +62,6 @@ func main() {
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
