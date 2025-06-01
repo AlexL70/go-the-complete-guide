ee1f63fe89e26943f25fddaaca2e65fdcc1271e7
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-06-01 13:27:46 +0300

    120. Mutating Maps

--- maps/maps.go
+++ maps/maps.go
@@ -12,5 +12,14 @@ func main() {
 		"Amazon Web Services": "https://aws.com",
 		"GitHub":              "https://github.com",
 	}
+	// Print the map
 	fmt.Println("Websites:", websites)
+	// Accessing a specific value
+	fmt.Println("AWS URL:", websites["Amazon Web Services"])
+	// Adding a new key-value pair
+	websites["LinkedIn"] = "https://www.linkedin.com"
+	fmt.Println("Updated Websites:", websites)
+	// removing an existing key-value pair
+	delete(websites, "GitHub")
+	fmt.Println("After deletion:", websites)
 }
