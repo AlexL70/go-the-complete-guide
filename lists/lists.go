275dc06fca210d4897df0e9ef26b3f60675109a9
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-06-01 12:05:26 +0300

    112. Selecting Parts of Arrays With Slices

--- lists/lists.go
+++ lists/lists.go
@@ -8,5 +8,8 @@ func main() {
 	prices := [4]float64{10.99, 20.49, 5.99, 15.00}
 	fmt.Println("Prices:", prices)
 	fmt.Println("Product names:", productNames)
-	fmt.Println(prices[2])
+	fmt.Println("Third price:", prices[2])
+	// Slicing the array
+	featuredPrices := prices[1:3] // Slicing from index 1 to 2 (exclusive)
+	fmt.Println("Featured prices:", featuredPrices)
 }
