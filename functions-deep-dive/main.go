a0ecd81d4318375bbed8bed02bb51457e8c45e74
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-06-02 12:49:45 +0300

    128. Returning Functions As Values

--- functions-deep-dive/main.go
+++ functions-deep-dive/main.go
@@ -6,10 +6,15 @@ type TransformInt func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2, 3}
 	doubles := transformNumbers(&numbers, double)
 	fmt.Println("Doubled:", doubles)
 	triples := transformNumbers(&numbers, triple)
 	fmt.Println("Tripled:", triples)
+	transformed1 := transformNumbers(&numbers, getTransformerFn(&numbers))
+	fmt.Println("Transformed 1:", transformed1)
+	transformed2 := transformNumbers(&moreNumbers, getTransformerFn(&moreNumbers))
+	fmt.Println("Transformed 2:", transformed2)
 }
 
 func transformNumbers(numbers *[]int, transform TransformInt) []int {
@@ -18,6 +23,14 @@ func transformNumbers(numbers *[]int, transform TransformInt) []int {
 		dNumbers[ind] = transform(val)
 	}
 	return dNumbers
+}
+
+func getTransformerFn(numbers *[]int) TransformInt {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
