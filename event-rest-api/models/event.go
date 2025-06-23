ad83a530572b9b94673500f6f182746c6ac42fed
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-06-23 17:39:06 +0300

    166. Testing Requests & Fixing the POST Request Handler

--- event-rest-api/models/event.go
+++ event-rest-api/models/event.go
@@ -7,7 +7,7 @@ type Event struct {
 	Name        string    `json:"name" binding:"required"`
 	Description string    `json:"description" binding:"required"`
 	Location    string    `json:"location" binding:"required"`
-	DateTime    time.Time `json:"date_time" binding:"required"`
+	DateTime    time.Time `json:"date" binding:"required"`
 	UserID      int       `json:"user_id"`
 }
 
