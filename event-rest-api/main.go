ad33ee6eb7f99071914489bfaf9e854ed3a6ec30
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-06-23 17:16:42 +0300

    165. Registering a POST Route

--- event-rest-api/main.go
+++ event-rest-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"event-rest-api/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,11 +10,25 @@ import (
 func main() {
 	var server = gin.Default()
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 	server.Run(":8080") // Start the server on port 8080 (on the local machine)
 }
 
 func getEvents(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello! Eventually, this endpoint will return a list of events",
-	})
+	events := models.GetAllEvents()
+	c.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, event)
 }
