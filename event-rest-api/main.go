9c8d12ce8ad54c7239e00e1ad513090c79bf69b5
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-06-24 09:14:14 +0300

    173. Getting Single Event Data By ID

--- event-rest-api/main.go
+++ event-rest-api/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"event-rest-api/db"
 	"event-rest-api/models"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,17 +14,33 @@ func main() {
 	db.InitDB()
 	var server = gin.Default()
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEventById)
 	server.POST("/events", createEvent)
 	server.Run(":8080") // Start the server on port 8080 (on the local machine)
 }
 
-func getEvents(c *gin.Context) {
-	events, err := models.GetAllEvents()
+func getEventById(context *gin.Context) {
+	id_str := context.Param("id")
+	id, err := strconv.ParseInt(id_str, 10, 64)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		context.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Invalid ID: %s. You must pass an integer value.", id_str)})
 		return
 	}
-	c.JSON(http.StatusOK, events)
+	event, err := models.GetEventById(id)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, event)
+}
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, events)
 }
 
 func createEvent(context *gin.Context) {
--- event-rest-api/models/event.go
+++ event-rest-api/models/event.go
@@ -54,3 +54,14 @@ func GetAllEvents() ([]Event, error) {
 	}
 	return events, nil
 }
+
+func GetEventById(id int64) (*Event, error) {
+	query := `SELECT id, name, description, location, date_time, user_id FROM events WHERE id = ?`
+	row := db.DB.QueryRow(query, id)
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+	if err != nil {
+		return nil, err
+	}
+	return &event, nil
+}
