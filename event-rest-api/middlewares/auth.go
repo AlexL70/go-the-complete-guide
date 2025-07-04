0638af29ea7ac0f769b6118743d5f7d7c7a27548
Alexander Levinson <alexander.levinson.70@gmail.com>
2025-07-04 11:35:43 +0300

    190. Adding an Authentication Middleware

---
+++ event-rest-api/middlewares/auth.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"event-rest-api/utils"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization token is required"})
+		return
+	}
+
+	claims, err := utils.ValidateToken(token)
+	if err != nil {
+		log.Printf("Error validating token: %v", err)
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization token is required"})
+		return
+	}
+
+	email := (*claims)["email"].(string)
+	userId := (*claims)["userId"].(float64)
+
+	context.Set("userId", userId)
+	context.Set("email", email)
+	context.Next()
+}
